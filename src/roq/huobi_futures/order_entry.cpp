2522979fe82f31bed1c5ab5230aa887d80599271
thraneh <thraneh@gmail.com>
2021-08-26 06:15:06 +0200

    using NotImplemented and NotSupported exceptions (clean-up)

--- src/roq/huobi_futures/order_entry.cpp
+++ src/roq/huobi_futures/order_entry.cpp
@@ -499,11 +499,11 @@ void OrderEntry::cancel_order(
 }
 
 void OrderEntry::operator()(const json::NewOrder &) {
-  log::fatal("NOT IMPLEMENTED"_sv);
+  throw NotImplementedException();
 }
 
 void OrderEntry::operator()(const json::CancelOrder &) {
-  log::fatal("NOT IMPLEMENTED"_sv);
+  throw NotImplementedException();
 }
 
 void OrderEntry::operator()(const json::ListenKey &listen_key) {
