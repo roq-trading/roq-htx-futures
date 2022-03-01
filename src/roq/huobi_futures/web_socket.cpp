043ed4d09a1a1cb00e21c4cf97349ddf47a85e49
thraneh <thraneh@gmail.com>
2022-03-01 10:23:51 +0100

    Authentication must support a redundant configuration (#193)

--- src/roq/huobi_futures/web_socket.cpp
+++ src/roq/huobi_futures/web_socket.cpp
@@ -36,9 +36,10 @@ struct create_metrics final : public core::metrics::Factory {
 };
 
 auto create_connection(auto &handler, auto &context) {
+  auto uri = Flags::ws_index_uri();
   core::web::ClientSocket::Config config{
       .validate_certificate = server::Flags::tls_validate_certificate(),
-      .uri = Flags::ws_index_uri(),
+      .uris = {&uri, 1},
       .query = {},
       .ping_frequency = Flags::ws_ping_freq(),
       .read_buffer_size = Flags::decode_buffer_size(),
