ba479fd66827af8573f99808b8e36b8e16b98ddc
thraneh <thraneh@gmail.com>
2022-04-21 14:39:10 +0200

    chore

--- src/roq/huobi_futures/market_data.cpp
+++ src/roq/huobi_futures/market_data.cpp
@@ -314,6 +314,7 @@ void MarketData::operator()(const Trace<json::BBO> &event) {
         },
         .update_type = UpdateType::INCREMENTAL,
         .exchange_time_utc = utils::safe_cast(bbo.ts),
+        .exchange_sequence = {},
     };
     create_trace_and_dispatch(handler_, trace_info, top_of_book, true);
   });
