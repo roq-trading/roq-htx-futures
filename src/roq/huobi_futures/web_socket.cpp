d2e6ef9d28dabed2c7d314394b951b90d98caf3d
thraneh <thraneh@gmail.com>
2022-04-21 10:03:41 +0200

    StreamStatus has been extended with more detailed information

--- src/roq/huobi_futures/web_socket.cpp
+++ src/roq/huobi_futures/web_socket.cpp
@@ -180,9 +180,11 @@ void WebSocket::operator()(ConnectionStatus status) {
         .stream_id = stream_id_,
         .account = {},
         .supports = SUPPORTS,
-        .status = status_,
-        .type = StreamType::WEB_SOCKET,
+        .transport = Transport::TCP,
+        .protocol = Protocol::HTTP,
+        .encoding = Encoding::JSON,
         .priority = Priority::PRIMARY,
+        .connection_status = status_,
     };
     log::info("stream_status={}"sv, stream_status);
     create_trace_and_dispatch(handler_, trace_info, stream_status);
