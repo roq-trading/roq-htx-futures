28539b5f718663985811ebd90cfe0260b9cb8a39
thraneh <thraneh@gmail.com>
2022-07-06 16:33:33 +0200

    fmt9

--- src/roq/huobi_futures/config.hpp
+++ src/roq/huobi_futures/config.hpp
@@ -67,7 +67,7 @@ struct fmt::formatter<roq::huobi_futures::Config> {
     return std::begin(context);
   }
   template <typename Context>
-  auto format(roq::huobi_futures::Config const &value, Context &context) {
+  auto format(roq::huobi_futures::Config const &value, Context &context) const {
     using namespace std::literals;
     return fmt::format_to(
         context.out(),
