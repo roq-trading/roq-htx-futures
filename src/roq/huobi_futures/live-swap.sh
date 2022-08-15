4caf882d816dfaf0e7c1d7622ece05ff84ef18f3
thraneh <thraneh@gmail.com>
2022-08-15 15:45:57 +0200

    update scripts

--- src/roq/huobi_futures/live-swap.sh
+++ src/roq/huobi_futures/live-swap.sh
@@ -21,8 +21,10 @@ WS_ORDER_URI="wss://$URI/swap-notification"
 $PREFIX ./roq-huobi-futures \
 	--name "huobi-futures" \
 	--config_file "$CONFIG_FILE" \
-	--client_listen_address $CWD/$NAME.sock \
-	--metrics_listen_address 2345 \
+  --event_log_dir "$HOME/var/lib/roq/data" \                                                                            
+  --event_log_symlink \                                                                                                 
+  --client_listen_address "$HOME/run/$NAME.sock" \                                                                      
+  --metrics_listen_address "$HOME/run/${NAME}_metrics.sock" \
 	--rest_uri "$REST_URI" \
 	--ws_market_uri "$WS_MARKET_URI" \
 	--ws_order_uri "$WS_ORDER_URI" \
