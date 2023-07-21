3caf8ef0b188e0443d00a446bcf5365f31fe079a
thraneh <thraneh@gmail.com>
2023-07-21 09:20:40 +0200

    remove config files from gateway projects

--- src/roq/huobi_futures/live-swap.sh
+++ src/roq/huobi_futures/live-swap.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-CWD="$(realpath "$(dirname "${BASH_SOURCE[0]}")")"
-
 if [ "$1" == "debug" ]; then
   PREFIX="gdb --args"
 else
@@ -10,7 +8,9 @@ fi
 
 NAME="huobi-futures"
 
-CONFIG_FILE="$CWD/config/$NAME.toml"
+CONFIG="${CONFIG:-$NAME}"
+
+CONFIG_FILE="$ROQ_CONFIG_PATH/roq-huobi-futures/$CONFIG.toml"
 
 URI="api.hbdm.com"
 
