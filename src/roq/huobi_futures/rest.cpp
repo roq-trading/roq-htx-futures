6eb47df44f5f7d73fc13ab8aef4742d8da3781b2
thraneh <thraneh@gmail.com>
2024-10-12 13:12:39 +0200

    adding settlement currency to reference data (#468) abd quantiy type to order management (#469)

--- src/roq/huobi_futures/rest.cpp
+++ src/roq/huobi_futures/rest.cpp
@@ -265,6 +265,7 @@ void Rest::operator()(Trace<json::ContractInfo> const &event) {
         .security_type = {},
         .base_currency = {},
         .quote_currency = {},
+        .settlement_currency = {},
         .margin_currency = {},
         .commission_currency = {},
         .tick_size = item.price_tick,
