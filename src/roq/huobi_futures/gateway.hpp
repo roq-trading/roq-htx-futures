bf503e3e5bc00ba828221da813bbb495c745f24a
thraneh <thraneh@gmail.com>
2025-01-17 09:34:06 +0100

    support Control and StatusUpdate (#484)

--- src/roq/huobi_futures/gateway.hpp
+++ src/roq/huobi_futures/gateway.hpp
@@ -42,6 +42,7 @@ struct Gateway final : public server::Handler,
   void operator()(Event<Start> const &) override;
   void operator()(Event<Stop> const &) override;
   void operator()(Event<Timer> const &) override;
+  void operator()(Event<Control> const &) override;
   void operator()(Event<Connected> const &) override;
   void operator()(Event<Disconnected> const &) override;
 
