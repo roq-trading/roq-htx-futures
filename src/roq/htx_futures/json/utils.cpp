ab66d90e0f8ee9a1154a9b37077118ee06691c65
thraneh <thraneh@gmail.com>
2026-01-12 09:37:15 +0100

    chore

--- src/roq/htx_futures/json/utils.cpp
+++ src/roq/htx_futures/json/utils.cpp
@@ -12,11 +12,11 @@ namespace json {
 
 // === IMPLEMENTATION ===
 
-Error guess_error(int32_t err_code) {
+Error guess_error([[maybe_unused]] int32_t err_code) {
   return {};
 }
 
-Error guess_error(std::string_view const &message) {
+Error guess_error([[maybe_unused]] std::string_view const &message) {
   return {};
 }
 
