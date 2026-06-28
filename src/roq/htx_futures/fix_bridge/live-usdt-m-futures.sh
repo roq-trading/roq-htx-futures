88b9480fe4cd84b20d2ef0ec4008c5269df6ca29
thraneh <thraneh@gmail.com>
2026-06-28 02:58:22 +0000

    moving fix_bridge into roq-server

---
+++ src/roq/htx_futures/fix_bridge/live-usdt-m-futures.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+if [ "$1" == "debug" ]; then
+  PREFIX="gdb --args"
+else
+  PREFIX=
+fi
+
+NAME="htx-futures"
+
+CONFIG="${CONFIG:-$NAME}"
+
+CONFIG_FILE="$ROQ_CONFIG_PATH/roq-htx-futures/$CONFIG.toml"
+
+API="usdt-m-futures"
+
+WS_API=true
+
+FLAGFILE="../../../../share/flags/prod/flags-$API.cfg"
+
+$PREFIX ./roq-htx-futures-fix-bridge \
+  --name "htx-futures" \
+  --config_file "$CONFIG_FILE" \
+  --flagfile "$FLAGFILE" \
+  --cache_dir "$HOME/var/lib/roq/cache" \
+  --event_log_dir "$HOME/var/lib/roq/data" \
+  --client_listen_address "$HOME/run/$NAME.sock" \
+  --service_listen_address "$HOME/run/metrics/${NAME}.sock" \
+  --api="$API" \
+  --ws_api=$WS_API \
+  $@
